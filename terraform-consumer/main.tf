71d12bae3ac1de5142e964e5ea94034b5939e402
Eshant Arora <eshant.arora@hashicorp.com>
2025-04-02 13:22:22 +0530

    Update main.tf

--- terraform-consumer/main.tf
+++ terraform-consumer/main.tf
@@ -1,5 +1,6 @@
 # main.tf (in the consumer project)
 module "custom_module" {
-  source = "github.com/Eshant-TFC/my-custom-terraform-module?ref=v1.0.0"
+  source  = "app.terraform.io/eshant-hcp-tf-test/Testing/aws"
+  version = "1.0.0"
 }
 
